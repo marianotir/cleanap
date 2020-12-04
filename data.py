443ead3b48e583c9e23fde4682cfa84dfdbdae82
mariano <marianotiradoalonso@gmail.com>
2020-12-04 16:41:49 +0100

    data analysis framework definition

--- data.py
+++ data.py
@@ -1,3 +1,77 @@
 
 
 
+#-------------------------------------
+# Import libraries
+#-------------------------------------
+
+import csv
+import xldr as xl
+import numpy as np
+import pandas as pd
+import matplotlib as pl
+
+
+#---------------------------------
+# Load data 
+#---------------------------------
+
+
+#-------------------------------
+# Data analysis 
+#-------------------------------
+
+
+# Number of columns
+
+
+# Type of data: Categorical or numerical
+
+
+# Consistency: Number of Na
+
+
+# Clean data from Na using imputer 
+
+
+
+#-------------------------------
+# Data plotting  
+#-------------------------------
+
+# Histogramas
+
+
+# Densisty plots
+
+
+# Correlation plots 
+
+
+
+#-------------------------------
+# Data preparation  
+#-------------------------------
+
+
+# Correlation of data
+
+
+# Determine possible target variable
+
+
+# Prepare data for no supervised analysis
+
+
+# Prepare data for PCA 
+
+
+# Prepare categorical data as nummy
+
+
+# Prepare data for entering a machine learning model 
+
+
+# Prepare data for timeseries analysis 
+
+
